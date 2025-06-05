e4ecf9e8741039a9607ec42b064ecf45d446a851
bbanachowicz <bbanachowicz@egnyte.com>
2025-06-05 16:58:55 +0200

    CFS-69435 | Refactor server.py

--- server.py
+++ server.py
@@ -1,17 +1,11 @@
-from typing import Any
-import httpx
+import os
+
+from dotenv import load_dotenv
+from egnyte import client
 from mcp.server.fastmcp import FastMCP
-from requests.auth import HTTPBasicAuth
-from typing import List
-from egnyte import configuration, client
-# from import domain, access_token
 
 # Initialize FastMCP server
 mcp = FastMCP("Egnyte Document Store Retriever")
-
-
-import os
-from dotenv import load_dotenv
 
 # Load .env file
 load_dotenv()
@@ -20,16 +14,12 @@ load_dotenv()
 domain = os.getenv('DOMAIN')
 access_token = os.getenv('ACCESS_TOKEN')
 
-
-client = client.EgnyteClient({"domain": domain,
-    "access_token": access_token
-})
-
+client = client.EgnyteClient({"domain": domain, "access_token": access_token})
 
 
 @mcp.tool()
 async def search_for_document_by_name(file_name: str) -> str:
-    """Searche an Egnyte domain for a document by name and returns the document text snippet"""
+    """Search an Egnyte domain for a document by name and returns the document text snippet"""
     search_results = client.search.files(file_name)
     return search_results[0].snippet
 
